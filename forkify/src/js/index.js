043cde62968789c2d4ff3d300466f301e0ee03a9
KuangDaiYouShang <natsu_kageusa@163.com>
2019-06-15 21:51:51 +0900

    Prject preparations#2
    node, npm, babel setup and configuration

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,4 +1,8 @@
 // Global app controller
-import num from './test';
+import str from './views/searchViews';
 
-console.log(`I imported ${num} from another module`);
+//import {add, multiply, ID} from './models/Search';
+
+import * as searchView from './models/Search';
+
+console.log(`Using imported function ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${str}`);
---
+++ forkify/src/js/views/searchViews.js
@@ -0,0 +1,1 @@
+export default 'I am the default export';
